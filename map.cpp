9db0fd452be07ed82b6056f7ff7ed6080819ddf1
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-11 23:55:05 +0600

    updated

--- map.cpp
+++ map.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
 map<int , int> mpp;
 mpp[1] = 2;
-mpp.emplace({2,4});
+//mpp.emplace({2,4});
 mpp.insert({5,4});
-mpp.insert({8,4});
+mpp.insert({8,5});
+mpp.insert({10,8});
+
 return 0;
 }
